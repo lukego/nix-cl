45a5c3fa52b87fd5fd96835ae0870be104015f7a
Luke Gorrie <luke@nuddy.net>
2022-11-25 14:31:48 +0100

    builder.lisp: Print more error details

--- builder.lisp
+++ builder.lisp
@@ -7,7 +7,8 @@
         (dolist (s (uiop:split-string systems :separator " "))
           (asdf:load-system s)))
     (error (c)
-      (format t "BUILD FAILED: ~A~%" c)
+      (format t "~&BUILD FAILED: ~S: ~A~%" (class-name (class-of c)) c)
+      (describe c)
       (uiop:quit 1)))
   (uiop:quit 0))
 
