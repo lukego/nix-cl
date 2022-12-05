d08f1c4768af7d8e5755e091d0aca75fc9a85d39
Luke Gorrie <luke@nuddy.net>
2022-12-05 14:53:30 +0100

    flake.nix: Bump nixpkgs to have shaderc upgrade

--- flake.nix
+++ flake.nix
@@ -2,20 +2,21 @@
 
   description = "Utilities for packaging ASDF Common Lisp systems";
 
-  inputs.dev.url = "github:uthar/dev";
+  #inputs.dev.url = "github:uthar/dev";
+  inputs.nixpkgs.url = "github:lukego/nixpkgs/shaderc-2022.4";
 
-  outputs = { self, dev, flake-utils }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      nixpkgs = dev.inputs.nixpkgs;
       pkgs = nixpkgs.legacyPackages.${system};
-      devpkgs = dev.outputs.packages.${system};
-      callWithLisps = x: pkgs.callPackage x { inherit (devpkgs) abcl clasp sbcl; };
+      #devpkgs = nixpkgs.outputs.packages.${system};
+      callWithLisps = x: pkgs.callPackage x { inherit (nixpkgs) abcl clasp sbcl; };
       lisps = callWithLisps ./.;
     in
     {
       packages = { inherit (lisps) abcl ccl clasp clisp ecl sbcl; };
       devShells.default = callWithLisps ./shell.nix;
+      pkgs = pkgs;
     });
 
 }
